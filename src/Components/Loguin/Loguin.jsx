e377375124630f613df0a5f55dbe4367853d4e0e
jesus148 <jorgecop149@gmail.com>
2024-07-24 13:08:22 -0500

    jesus

--- src/Components/Loguin/Loguin.jsx
+++ src/Components/Loguin/Loguin.jsx
@@ -14,18 +14,18 @@ function Loguin() {
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text text-xl text-orange-50 flex justify-start'>
+        <div className='text text-3xl text-orange-50 flex justify-start text-whiteTwo font-extrabold'>
               {action}
         </div>
          <div className='underline'></div>
       </div>
       <div className='inputs'>
-        <div className='input flex columns-2'>
+        <div className='input flex columns-2 '>
           <div className='icon w-1/6'>
         <FaUser />
           </div>
           <div className='w-5/6' >
-          <input type="text" placeholder='tu usuario' className='bg-[#3e4957]' />
+          <input type="text" placeholder='Usuario' className='bg-[#3e4957]  border-b border-yellow-400' />
 
           </div>
 
@@ -36,19 +36,26 @@ function Loguin() {
         <RiLockPasswordFill />
           </div>
           <div className='w-5/6'>
-            <input type="password" placeholder='Contraseña' className='bg-[#3e4957]  border-b-4'/> 
+            <input type="password" placeholder='Contraseña' className='bg-[#3e4957]  border-b
+            border-yellow-400'/> 
 
           </div>
         </div>
 
       </div>
-      <div className='forgot-password '>Olvido Contraseña</div>
-      <div className='submit-container flex flex-row justify-around'>
-        <div className={action==="Sign Up"?"submit gray":"submit"}  onclick={()=>{setAction("Sign Up")}}>Registrarse</div>
+    
+      
+      <div className='submit-container flex flex-row justify-evenly'>
+        <button className={action==="LOG IN"?"submit gray rounded-full":"submit" } 
+         
+        onclick={()=>{setAction("Sign Up")}}>Registrarse</button>
 
 
-        <div className={action==="Loguin"?"submit gray":"submit"} onclick={()=>{setAction("Loguin")}}>Entrar</div>
+        <button className={action==="Sign Up"?"submit gray rounded-full":"submit rounded-full"} onclick={()=>{setAction("Loguin")}}>Entrar</button>
       </div>
+
+
+      <div className='forgot-passwor flex justify-center m-5 text-white gap-1 font-semibold'>Olvido Contraseña <a href="" className='text-blue-200'> Aqui</a> </div>
 
     </div>
   )
